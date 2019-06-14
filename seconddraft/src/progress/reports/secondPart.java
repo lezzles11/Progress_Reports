acd2305a79fad62bb6e2e8654049fee4eb2bf845
Lesley Cheung <lesleyc@bu.edu>
2019-06-14 14:07:59 -0400

    Testing out the second part

--- seconddraft/src/progress/reports/secondPart.java
+++ seconddraft/src/progress/reports/secondPart.java
@@ -18,6 +18,7 @@ public class secondPart {
     private String nameOfClass; 
     private String heshe; 
     private String hisher; 
+    private ArrayList<String> goalAreas; 
     
     public secondPart(String name, HashMap<String, ArrayList<String>> benchmarks) {
         this.name = name; 
@@ -27,6 +28,7 @@ public class secondPart {
         this.nameOfClass = new String();
         this.hisher = new String(); 
         this.heshe = new String(); 
+        this.goalAreas = new ArrayList<String>(); 
     } 
     // What class?
     public void secondPart1(){
@@ -86,19 +88,41 @@ public class secondPart {
     }
     
     public void secondPart5(){
-        for (String benchmark : this.benchmarks.keySet()) {
-            
-            System.out.println("How did the student do on ");
-        }
-        System.out.println("How did the student do on the goal?");
+        Scanner reader = new Scanner(System.in); 
+        String get = new String(); 
+        for (int i = 0; i < this.benchmarks.size(); i++) {
+            int num = i + 1; 
+            System.out.println("Is goal " + num + " an [1] organization, [2] writing, [3] reading or [4] math goal?");
+            System.out.println("Enter a number, please");
+            System.out.println("Remember, goal " + num + " is " + this.benchmarks.get(i));
+            int goalArea = Integer.parseInt(reader.nextLine()); 
+            if (goalArea == 1) {
+                get = getOrganization(); 
+            } else if (goalArea == 2) {
+                get = getWriting(); 
+            } else if (goalArea == 3) {
+                get = getReading(); 
+            } else if (goalArea == 4) {
+                get = getMath(); 
+            } else {
+                System.out.println("Sorry, please type in a number");
+            }
+    }this.goalAreas.add(get); 
     }
     
+    public void secondPart6() {
+        Scanner reader = new Scanner(System.in); 
+        System.out.println("How did " + this.name + " perform on the goal?");
+        System.out.println("[1] Awesome!! [2] Okay (making progress) [3] Really made no progress / struggled in making progress");
+        int num = Integer.parseInt(reader.nextLine());
+        
+    }
     public void great(String goalArea){
         this.benchmarks.forEach((k, v) -> {
             System.out.print(k + " = ");
             v.forEach(w -> System.out.print(w + ","));
             System.out.println(this.name + " exceed in " + goalArea);
-            System.out.println(this.name + " has been a positive influence on the class, and we are proud to see " + this.name +"'s progress over the past quarter");
+            System.out.println(this.name + " has been a positive influence on the class, and we are proud to see " + this.name +"'s progress over the past quarter("+ this.initials +").");
             System.out.println("");
             System.out.println();
         });
@@ -111,7 +135,7 @@ public class secondPart {
             System.out.println(this.name + " did well in " + goalArea);
             System.out.println(this.heshe + " may need to continue to work hard on all assignments (as well as completing them on time).");
             System.out.println("Based on " + this.name + "'s current rate of progress, and with continued direct instruction and support, ");
-            System.out.println(this.name + "should be able to achieve this annual goal by the end of the IEP period.");
+            System.out.println(this.name + "should be able to achieve this annual goal by the end of the IEP period ("+ this.initials +").");
             System.out.println();
         });
     }
@@ -127,22 +151,22 @@ public class secondPart {
         });
     }
     
-    public String organization() {
+    public String getOrganization() {
         String organization = " utilizing an organizational system in checking with " + this.hisher + " teachers";
         return organization; 
     }
     
-    public String reading() {
+    public String getReading() {
         String reading = " reading to identify the main idea of various passages.";
         return reading; 
     }
     
-    public String writing(){
+    public String getWriting(){
         String writing = " writing a cohesive multi-paragraph writing piece.";
         return writing; 
     }
     
-    public String math() {
+    public String getMath() {
         String math = " solving complex word problems by breaking each step down."; 
         return math; 
     }
